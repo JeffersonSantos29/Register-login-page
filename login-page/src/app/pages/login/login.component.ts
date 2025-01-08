577d0fe65a6cc5bf8848cc626ed4b4df572b40f1
jeffersonsantos30 <jeferson29santos@gmail.com>
2025-01-07 21:20:24 -0300

    style: Improve SCSS structure and styling consistency

--- login-page/src/app/pages/login/login.component.ts
+++ login-page/src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { DefaultLoginLayoutComponent } from '../../../components/default-login-layout/default-login-layout.component';
+import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 
 @Component({
   selector: 'app-login',
