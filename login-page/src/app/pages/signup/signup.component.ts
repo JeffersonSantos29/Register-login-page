668931b3839cd32bb1645bfa8c5299d039fbc3ff
JeffersonSantos29 <jeffersonalfredosa30@gmail.com>
2025-01-29 18:30:32 -0300

    feat: completed color updates in SCSS
    - Added variables for color palette
    - Adjusted mixins and functions for efficient color handling
    - Optimized style structure with modularization

--- login-page/src/app/pages/signup/signup.component.ts
+++ login-page/src/app/pages/signup/signup.component.ts
@@ -8,13 +8,15 @@ import { ToastrService } from 'ngx-toastr';
 
 
 
-interface LoginForm {
+interface SignupForm {
+  name: FormControl,
   email: FormControl,
-  password: FormControl
+  password: FormControl,
+  passwordConfirm: FormControl
 }
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-signup',
   standalone: true,
   imports: [
     DefaultLoginLayoutComponent,
@@ -22,30 +24,34 @@ interface LoginForm {
     PrimaryInputComponent
 
   ],
-  providers: [LoginService],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  providers: [
+    LoginService
+  ],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
 })
-export class LoginComponent {
-  loginForm!: FormGroup<LoginForm>;
+export class SignupComponent {
+  signupForm!: FormGroup<SignupForm>;
 
     constructor(
       private router: Router,
       private loginService: LoginService,
       private toastService: ToastrService
     ){
-      this.loginForm = new FormGroup({
+      this.signupForm = new FormGroup({
+        name: new FormControl('', [Validators.required, Validators.minLength(3)]),
         email: new FormControl('', [Validators.required, Validators.email]),
-        password: new FormControl('', [Validators.required, Validators.minLength(6)])
+        password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+        passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)])
       })
   }
   submit(){
-      this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
         next: () => this.toastService.success("Login Feito com sucesso!"),
         error: () => this.toastService.error("Erro inesperado!")
       })
   }
   navigate(){
-    this.router.navigate(["signup"])
+    this.router.navigate(["login"])
   }
 }
